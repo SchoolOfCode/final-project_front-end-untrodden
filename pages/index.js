374bfe7e3c54ad81e0811f82d14b12216be32cf4
danielhawkesford <daniel.hawkesford@gmail.com>
2022-07-26 17:10:48 +0100

    Removed access token test from index page

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,6 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
-          <p>{process.env.NEXT_PUBLIC_MAP_ACCESS_TOKEN}</p>
         </h1>
 
         <p className={styles.description}>
