18003a407dae506702d7436283c1df28b2b99473
TomBooth <thomas.alexander.booth@gmail.com>
2022-07-26 18:37:23 +0100

    added decided style to map

--- Components/map.js
+++ Components/map.js
@@ -6,16 +6,16 @@ export default function Map() {
         width: "100%",
         height: "100%",
         // The latitude and longitude of the center of London
-        latitude: 51.5074,
-        longitude: -0.1278,
+        latitude: 52.489471,
+        longitude: -1.898575,
         zoom: 10
     });
 
     return (
         <ReactMapGL
-            mapStyle="mapbox://styles/mapbox/streets-v11"
-            mapboxAccessToken={process.env.NEXT_PUBLIC_MAP_ACCESS_TOKEN}
-            // mapboxAccessToken="pk.eyJ1IjoidG9tYm9vdGg0NTMiLCJhIjoiY2w2MHN1eHRzMDNocDNqanZraHdnODk3MiJ9.v1i7uaU_HioJgFeF0REClw"
+            mapStyle="mapbox://styles/tombooth453/cl622p3gs000914s5r8fg0jak"
+            // mapboxAccessToken={process.env.NEXT_PUBLIC_MAP_ACCESS_TOKEN}
+      mapboxAccessToken="pk.eyJ1IjoidG9tYm9vdGg0NTMiLCJhIjoiY2w2MHN1eHRzMDNocDNqanZraHdnODk3MiJ9.v1i7uaU_HioJgFeF0REClw"
             {...viewport}
             onViewportChange={(nextViewport) => setViewport(nextViewport)}
             >
