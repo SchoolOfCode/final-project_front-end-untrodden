b6acfb9b9d340911badd0d7ce4202911d586df22
TomBooth <thomas.alexander.booth@gmail.com>
2022-07-26 19:35:38 +0100

    map is responsive down to approx 490pxw

--- pages/map.js
+++ pages/map.js
@@ -7,7 +7,7 @@ const MapComponent = dynamic(() => import("../Components/map.js"), {
   
 const Map = () => {
     return (
-        <div id='map' style={{height:'650px', width:'550px'}}>
+        <div id='map' style={{height:'80vmax', width:'80vmax' }}>
             <MapComponent />
         </div>
     );
