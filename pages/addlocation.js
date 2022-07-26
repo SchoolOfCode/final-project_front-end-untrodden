4ba2fd2095983f915530d3fae9d8a82e5e7888e7
Kieran <kieran.webdevelopment@gmail.com>
2022-07-26 14:53:20 +0100

    basic grid structure setup

--- pages/addlocation.js
+++ pages/addlocation.js
@@ -2,8 +2,16 @@ import styles from '../styles/addLocation.module.css';
 
 export default function Addlocation() {
   return (
-    <main>
+    <main className={styles.main}>
       <h1 className={styles.h1}>Add Location</h1>
+      <div className={styles.main_grid_container}>
+        <div className={styles.grid_left}>
+          <p>left</p>
+        </div>
+        <div className={styles.grid_right}>
+          <p>right</p>
+        </div>
+      </div>
     </main>
   );
 }
