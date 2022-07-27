d231ecd1d667e28facedb10f26ed3651352b4f49
Kieran <kieran.webdevelopment@gmail.com>
2022-07-27 18:15:47 +0100

    added more amenities to form feature

--- pages/addlocation.js
+++ pages/addlocation.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+// amenities pics
 import Parking from '../public/amenities_images/local_parking.png';
 import Food from '../public/amenities_images/food.png';
 import Family from '../public/amenities_images/carbon_pedestrian-family.png';
@@ -6,6 +7,15 @@ import BabyChanging from '../public/amenities_images/baby_changing_station.png';
 import Disability from '../public/amenities_images/accessible.png';
 import Peaceful from '../public/amenities_images/bxs_volume-mute.png';
 import Wifi from '../public/amenities_images/wifi.png';
+import NoRestaurants from '../public/amenities_images/no_meals.png';
+import Museums from '../public/amenities_images/museum.png';
+import Beach from '../public/amenities_images/beach.png';
+import Hiking from '../public/amenities_images/hiking.png';
+import PetFriendly from '../public/amenities_images/pet_friendly.png';
+import Nature from '../public/amenities_images/nature.png';
+import Wildlife from '../public/amenities_images/wildlife';
+import Kayaking from '../public/amenities_images/kayaking.png';
+import Shopping from '../public/amenities_images/shopping_bag.png';
 
 import styles from '../styles/addLocation.module.css';
 import { useForm } from 'react-hook-form';
@@ -212,154 +222,331 @@ export default function Addlocation() {
           {/* amenities section */}
 
           <div className={styles.grid_right}>
-            <label>Select all available Amenities</label>
+            <div className={styles.flex_container_right}>
+              <label>Select all available Amenities</label>
 
-            <div className={styles.amenities_category_grid_container}>
-              <div className={`${styles.amenities_category} ${styles.checked}`}>
-                <label>
-                  <input
-                    type="checkbox"
-                    id="amenities_parking"
-                    {...register('amenities_parking')}
-                  />{' '}
-                  <span>
-                    <Image
-                      className={styles.amenities_pic}
-                      src={Parking}
-                      height={35}
-                      width={35}
-                    ></Image>
-                  </span>
-                </label>
+              <div className={styles.amenities_category_grid_container}>
+                <div
+                  className={`${styles.amenities_category} ${styles.checked}`}
+                >
+                  <label>
+                    <input
+                      type="checkbox"
+                      id="amenities_parking"
+                      {...register('amenities_parking')}
+                    />
+                    <span>
+                      <Image
+                        className={styles.amenities_pic}
+                        src={Parking}
+                        height={35}
+                        width={35}
+                      ></Image>
+                    </span>
+                  </label>
+                </div>
+
+                <div
+                  className={`${styles.amenities_category} ${styles.checked}`}
+                >
+                  <label>
+                    <input
+                      type="checkbox"
+                      id="amenities_food"
+                      {...register('amenities_food')}
+                    />
+                    <span>
+                      <Image
+                        className={styles.amenities_pic}
+                        src={Food}
+                        height={35}
+                        width={35}
+                      ></Image>
+                    </span>
+                  </label>
+                </div>
+
+                <div
+                  className={`${styles.amenities_category} ${styles.checked}`}
+                >
+                  <label>
+                    <input
+                      type="checkbox"
+                      id="amenities_family"
+                      {...register('amenities_family')}
+                    />
+                    <span>
+                      <Image
+                        className={styles.amenities_pic}
+                        src={Family}
+                        height={35}
+                        width={35}
+                      ></Image>
+                    </span>
+                  </label>
+                </div>
+
+                <div
+                  className={`${styles.amenities_category} ${styles.checked}`}
+                >
+                  <label>
+                    <input
+                      type="checkbox"
+                      id="amenities_changing_facilities"
+                      {...register('amenities_changing_facilities')}
+                    />
+                    <span>
+                      <Image
+                        className={styles.amenities_pic}
+                        src={BabyChanging}
+                        height={35}
+                        width={35}
+                      ></Image>
+                    </span>
+                  </label>
+                </div>
+
+                <div
+                  className={`${styles.amenities_category} ${styles.checked}`}
+                >
+                  <label>
+                    <input
+                      type="checkbox"
+                      id="amenities_disability_access"
+                      {...register('amenities_disability_access')}
+                    />
+                    <span>
+                      <Image
+                        className={styles.amenities_pic}
+                        src={Disability}
+                        height={35}
+                        width={35}
+                      ></Image>
+                    </span>
+                  </label>
+                </div>
+
+                <div
+                  className={`${styles.amenities_category} ${styles.checked}`}
+                >
+                  <label>
+                    <input
+                      type="checkbox"
+                      id="amenities_peaceful"
+                      {...register('amenities_peaceful')}
+                    />
+                    <span>
+                      <Image
+                        className={styles.amenities_pic}
+                        src={Peaceful}
+                        height={35}
+                        width={35}
+                      ></Image>
+                    </span>
+                  </label>
+                </div>
+
+                <div
+                  className={`${styles.amenities_category} ${styles.checked}`}
+                >
+                  <label>
+                    <input
+                      type="checkbox"
+                      id="amenities_wifi"
+                      {...register('amenities_wifi')}
+                    />
+                    <span>
+                      <Image
+                        className={styles.amenities_pic}
+                        src={Wifi}
+                        height={35}
+                        width={35}
+                      ></Image>
+                    </span>
+                  </label>
+                </div>
+
+                <div
+                  className={`${styles.amenities_category} ${styles.checked}`}
+                >
+                  <label>
+                    <input
+                      type="checkbox"
+                      id="amenities_no_restaurants"
+                      {...register('amenities_no_restaurants')}
+                    />
+                    <span>
+                      <Image
+                        className={styles.amenities_pic}
+                        src={NoRestaurants}
+                        height={35}
+                        width={35}
+                      ></Image>
+                    </span>
+                  </label>
+                </div>
+
+                <div
+                  className={`${styles.amenities_category} ${styles.checked}`}
+                >
+                  <label>
+                    <input
+                      type="checkbox"
+                      id="amenities_hiking"
+                      {...register('amenities_hiking')}
+                    />
+                    <span>
+                      <Image
+                        className={styles.amenities_pic}
+                        src={NoRestaurants}
+                        height={35}
+                        width={35}
+                      ></Image>
+                    </span>
+                  </label>
+                </div>
+
+                <div
+                  className={`${styles.amenities_category} ${styles.checked}`}
+                >
+                  <label>
+                    <input
+                      type="checkbox"
+                      id="amenities_no_restaurants"
+                      {...register('amenities_no_restaurants')}
+                    />
+                    <span>
+                      <Image
+                        className={styles.amenities_pic}
+                        src={NoRestaurants}
+                        height={35}
+                        width={35}
+                      ></Image>
+                    </span>
+                  </label>
+                </div>
+
+                <div
+                  className={`${styles.amenities_category} ${styles.checked}`}
+                >
+                  <label>
+                    <input
+                      type="checkbox"
+                      id="amenities_no_restaurants"
+                      {...register('amenities_no_restaurants')}
+                    />
+                    <span>
+                      <Image
+                        className={styles.amenities_pic}
+                        src={NoRestaurants}
+                        height={35}
+                        width={35}
+                      ></Image>
+                    </span>
+                  </label>
+                </div>
+
+                <div
+                  className={`${styles.amenities_category} ${styles.checked}`}
+                >
+                  <label>
+                    <input
+                      type="checkbox"
+                      id="amenities_no_restaurants"
+                      {...register('amenities_no_restaurants')}
+                    />
+                    <span>
+                      <Image
+                        className={styles.amenities_pic}
+                        src={NoRestaurants}
+                        height={35}
+                        width={35}
+                      ></Image>
+                    </span>
+                  </label>
+                </div>
+
+                <div
+                  className={`${styles.amenities_category} ${styles.checked}`}
+                >
+                  <label>
+                    <input
+                      type="checkbox"
+                      id="amenities_no_restaurants"
+                      {...register('amenities_no_restaurants')}
+                    />
+                    <span>
+                      <Image
+                        className={styles.amenities_pic}
+                        src={NoRestaurants}
+                        height={35}
+                        width={35}
+                      ></Image>
+                    </span>
+                  </label>
+                </div>
+
+                <div
+                  className={`${styles.amenities_category} ${styles.checked}`}
+                >
+                  <label>
+                    <input
+                      type="checkbox"
+                      id="amenities_no_restaurants"
+                      {...register('amenities_no_restaurants')}
+                    />
+                    <span>
+                      <Image
+                        className={styles.amenities_pic}
+                        src={NoRestaurants}
+                        height={35}
+                        width={35}
+                      ></Image>
+                    </span>
+                  </label>
+                </div>
+
+                <div
+                  className={`${styles.amenities_category} ${styles.checked}`}
+                >
+                  <label>
+                    <input
+                      type="checkbox"
+                      id="amenities_no_restaurants"
+                      {...register('amenities_no_restaurants')}
+                    />
+                    <span>
+                      <Image
+                        className={styles.amenities_pic}
+                        src={NoRestaurants}
+                        height={35}
+                        width={35}
+                      ></Image>
+                    </span>
+                  </label>
+                </div>
+
+                <div
+                  className={`${styles.amenities_category} ${styles.checked}`}
+                >
+                  <label>
+                    <input
+                      type="checkbox"
+                      id="amenities_no_restaurants"
+                      {...register('amenities_no_restaurants')}
+                    />
+                    <span>
+                      <Image
+                        className={styles.amenities_pic}
+                        src={NoRestaurants}
+                        height={35}
+                        width={35}
+                      ></Image>
+                    </span>
+                  </label>
+                </div>
               </div>
-
-              <div className={`${styles.amenities_category} ${styles.checked}`}>
-                <label>
-                  <input
-                    type="checkbox"
-                    id="amenities_food"
-                    {...register('amenities_food')}
-                  />
-                  <span>
-                    <Image
-                      className={styles.amenities_pic}
-                      src={Food}
-                      height={35}
-                      width={35}
-                    ></Image>
-                  </span>
-                </label>
-              </div>
-
-              <div className={`${styles.amenities_category} ${styles.checked}`}>
-                <label>
-                  <input
-                    type="checkbox"
-                    id="amenities_family"
-                    {...register('amenities_family')}
-                  />
-                  <span>
-                    <Image
-                      className={styles.amenities_pic}
-                      src={Family}
-                      height={35}
-                      width={35}
-                    ></Image>
-                  </span>
-                </label>
-              </div>
-
-              <div className={`${styles.amenities_category} ${styles.checked}`}>
-                <label>
-                  <input
-                    type="checkbox"
-                    id="amenities_changing_facilities"
-                    {...register('amenities_changing_facilities')}
-                  />
-                  <span>
-                    <Image
-                      className={styles.amenities_pic}
-                      src={BabyChanging}
-                      height={35}
-                      width={35}
-                    ></Image>
-                  </span>
-                </label>
-              </div>
-
-              <div className={`${styles.amenities_category} ${styles.checked}`}>
-                <label>
-                  <input
-                    type="checkbox"
-                    id="amenities_disability_access"
-                    {...register('amenities_disability_access')}
-                  />
-                  <span>
-                    <Image
-                      className={styles.amenities_pic}
-                      src={Disability}
-                      height={35}
-                      width={35}
-                    ></Image>
-                  </span>
-                </label>
-              </div>
-
-              <div className={`${styles.amenities_category} ${styles.checked}`}>
-                <label>
-                  <input
-                    type="checkbox"
-                    id="amenities_peaceful"
-                    {...register('amenities_peaceful')}
-                  />
-                  <span>
-                    <Image
-                      className={styles.amenities_pic}
-                      src={Peaceful}
-                      height={35}
-                      width={35}
-                    ></Image>
-                  </span>
-                </label>
-              </div>
-
-              <div className={`${styles.amenities_category} ${styles.checked}`}>
-                <label>
-                  <input
-                    type="checkbox"
-                    id="amenities_wifi"
-                    {...register('amenities_wifi')}
-                  />
-                  <span>
-                    <Image
-                      className={styles.amenities_pic}
-                      src={Wifi}
-                      height={35}
-                      width={35}
-                    ></Image>
-                  </span>
-                </label>
-              </div>
-
-              {/* <div className={`${styles.amenities_category} ${styles.checked}`}>
-                <label>
-                  <input
-                    type="checkbox"
-                    id="amenities_food"
-                    {...register('amenities_food')}
-                  />
-                  <span>
-                    <Image
-                      className={styles.amenities_pic}
-                      src={Food}
-                      height={35}
-                      width={35}
-                    ></Image>
-                  </span>
-                </label>
-              </div> */}
             </div>
-            <p>right</p>
           </div>
         </div>
         <div className={styles.btn_container}>
