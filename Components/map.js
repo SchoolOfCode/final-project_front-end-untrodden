ea0addf452b23dbd34cd227d6042a79a325110e5
danielhawkesford <daniel.hawkesford@gmail.com>
2022-07-28 11:56:04 +0100

    Added lat and long states for the popup

--- Components/map.js
+++ Components/map.js
@@ -1,15 +1,14 @@
-
 import styles from '../styles/Map.module.css'
 import { useRef, useState } from "react";
 import ReactMapGL,{Marker, Popup} from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css"; 
-import mapboxgl from 'mapbox-gl';
 
 export default function Map() {
     const mapRef = useRef(null);
     const [showPopup, setShowPopup] = useState(false);
+    const [popupLat, setPopupLat] = useState(null);
+    const [popupLong, setPopupLong] = useState(null);
 
-    // const map =  new mapboxgl.Map({});
     const [viewport, setViewport] = useState({
         width: "100%",
         height: "100%",
@@ -18,48 +17,6 @@ export default function Map() {
         longitude: -3.898575,
         zoom: 5
     });
-//  attempt at event listener to check if the map is even registering the drag, but it doesnt like the const on line 9 ?????
-    // map.on('drag', () => {
-    //     console.log('A drag event occurred.');
-    //     });
-
-    const geojson = {
-        type: 'FeatureCollection',
-        features: [
-          {
-            type: 'Feature',
-            geometry: {
-              type: 'Point',
-              coordinates: [-77.032, 38.913]
-            },
-            properties: {
-              title: 'Mapbox',
-              description: 'Washington, D.C.'
-            }
-          },
-          {
-            type: 'Feature',
-            geometry: {
-              type: 'Point',
-              coordinates: [-122.414, 37.776]
-            },
-            properties: {
-              title: 'Mapbox',
-              description: 'San Francisco, California'
-            }
-          }
-        ]
-      };
-
-// add markers to map
-{/*for (const feature of geojson.features) {
-    // create a HTML element for each feature
-    const el = document.createElement('div');
-    el.className = 'marker';
-  
-    // make a marker for each feature and add to the map
-    new mapboxgl.Marker(el).setLngLat(feature.geometry.coordinates).addTo(Map); */}
-  
 
     return (
         <section className={styles.map_container}>
@@ -78,26 +35,25 @@ export default function Map() {
             >
 
               <Marker longitude={-2} latitude={54} anchor="bottom" >
-                <img src="/location-marker.png" onClick={() => setShowPopup(true)} />
+                <img src="/location-marker.png" onClick={() => {
+                    setPopupLat(54);
+                    setPopupLong(-2)
+                    setShowPopup(true)}} />
              </Marker>
 
     {showPopup && (
-      <Popup longitude={-2} latitude={54}
+      <Popup longitude={popupLong} latitude={popupLat}
               anchor="top-left"
         closeOnClick={false}
         onClose={() => setShowPopup(false)}>
         You are here
       </Popup>)}
 
-      {showPopup && (
-      <Popup longitude={-6.5} latitude={55}
-        anchor="top-left"
-        closeOnClick={false}
-        onClose={() => setShowPopup(false)}>
-        You are here
-      </Popup>)}
     <Marker longitude={-6.5} latitude={55} anchor="bottom" >
-      <img src="/location-marker.png" />
+      <img src="/location-marker.png" onClick={() => {
+                    setPopupLat(55);
+                    setPopupLong(-6.5)
+                    setShowPopup(true)}}/>
     </Marker>
         </ReactMapGL>
       
