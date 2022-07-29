1521c0d931a0ce804537c4f535f1fab63f54b587
glynk <glyn_knight@hotmail.com>
2022-07-29 09:46:33 +0100

    Removed filter comp from about us page- hot fix

--- pages/aboutUs.js
+++ pages/aboutUs.js
@@ -1,7 +1,7 @@
-import AmenitiesFilter from "../Components/Amenities Filter/amenitiesFilter"
+
 export default function AboutUs(){
     return <div>
         <h1>About Us</h1>
-        <AmenitiesFilter/>
+        
     </div>
 }
