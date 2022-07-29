4d9414a9c503fc540a43e8706843ce744f8e4767
Kieran <kieran.webdevelopment@gmail.com>
2022-07-29 13:47:21 +0100

    getting data :)

--- pages/location/[id].js
+++ pages/location/[id].js
@@ -1,17 +1,39 @@
-import styles from '../../styles/LocationDisplayPage.module.css'
-import Image from 'next/image'
+import styles from '../../styles/LocationDisplayPage.module.css';
+import Image from 'next/image';
 
-export default function LocationDisplayPage(){
-    return <div >
-    <section className={styles.location_info}>
-    <section>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    </section>
-    <section>
-    <h3>Old Pale Hill</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    </section>
-    
-    </section>
-</div>
+export async function getStaticPaths() {
+  const res = await fetch('https://untrodden.herokuapp.com/locations/');
+  const data = await res.json();
+
+  const paths = data.payload.map((location) => {
+    return {
+      params: { id: location.location_id.toString() },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const id = context.params.id;
+  console.log(id);
+  const res = await fetch('https://untrodden.herokuapp.com/locations/' + id);
+  const data = await res.json();
+  console.log(data);
+  return {
+    props: { location: data.payload[0] },
+  };
+}
+
+export default function LocationDisplayPage({ location }) {
+  return (
+    <div>
+      <h1>hello</h1>
+      <p>{location.location_name}</p>
+      <p>{location.street}</p>
+    </div>
+  );
 }
