ae26b826c89a95f22d3317bea772a64777dc48b4
TomBooth <thomas.alexander.booth@gmail.com>
2022-08-01 09:36:29 +0100

    added console logs to location page data

--- pages/location/[id].js
+++ pages/location/[id].js
@@ -48,13 +48,19 @@ export default function LocationDisplayPage({ location }) {
 </div>
 <div className={styles.right_div}>
       <h1>{location.location_name}</h1>
+      console.log(location.location_name)
       <h3 className={styles.heading}>Address</h3>
       <p>{location.street}</p>
+      console.log(location.street)
       <p>{location.town}</p>
+      console.log(location.town)
       <p>{location.region}</p>
+      console.log(location.region)
       <p>{location.postcode}</p>
+      console.log(location.postcode)
       <h3 className={styles.heading}>Description</h3>
       <p>{location.location_description}</p>
+      console.log(location.location_description)
 
   </div>   
     </div>
