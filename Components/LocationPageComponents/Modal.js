2e44ae00d4f96504a6980d8aebf773967709d108
Kieran <kieran.webdevelopment@gmail.com>
2022-08-03 14:07:43 +0100

    report form modal setup

---
+++ Components/LocationPageComponents/Modal.js
@@ -0,0 +1,109 @@
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { useState } from 'react';
+//formspree
+import { useForm, ValidationError } from '@formspree/react';
+
+import styles from '../../styles/LocationDisplayPage.module.css';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function TransitionsModal() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const [state, handleSubmit] = useForm('mzbwprql');
+
+  return (
+    <div className={styles.modal}>
+      <Button onClick={handleOpen}>
+        <div className={styles.report}>
+          <p>Report any issues with this page</p>
+          <img src="/report_flag.png" />
+        </div>
+        ;
+      </Button>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+            <Typography id="transition-modal-title" variant="h5" component="h2">
+              Report an Issue
+            </Typography>
+            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+              <form className="contact-form" onSubmit={handleSubmit}>
+                <label htmlFor="name">Name</label>
+                <input id="name" type="text" name="messager-name" />
+                <ValidationError
+                  prefix="Name"
+                  field="messager-name"
+                  errors={state.errors}
+                />
+
+                <label htmlFor="email">Email</label>
+                <input id="email" type="email" name="email" />
+                <ValidationError
+                  prefix="Email"
+                  field="email"
+                  errors={state.errors}
+                />
+
+                <label htmlFor="subject">Subject</label>
+                <input id="subject" type="text" name="message-subject" />
+                <ValidationError
+                  prefix="Subject"
+                  field="message-subject"
+                  errors={state.errors}
+                />
+                <label htmlFor="message">Message</label>
+                <textarea id="message" name="message" />
+                <ValidationError
+                  prefix="Message"
+                  field="message"
+                  errors={state.errors}
+                />
+
+                <button
+                  className="btn-main form-button"
+                  type="submit"
+                  disabled={state.submitting}
+                >
+                  Submit
+                </button>
+                <ValidationError errors={state.errors} />
+                {state.succeeded && (
+                  <h1>Thanks for your submission! You&apos;ve made my day!</h1>
+                )}
+              </form>
+            </Typography>
+          </Box>
+        </Fade>
+      </Modal>
+    </div>
+  );
+}
--- pages/location/[id].js
+++ pages/location/[id].js
@@ -1,9 +1,11 @@
 import styles from '../../styles/LocationDisplayPage.module.css';
 import Image from 'next/image';
-
+import Modal from '../../Components/LocationPageComponents/Modal';
 //map stuff
 import MapLocationPage from '../../Components/MapLocationPage/mapLocationPage';
 import dynamic from 'next/dynamic';
+// import { Modal } from '@mui/material';
+
 // working in this one
 
 // const MapComponent = dynamic(
@@ -43,20 +45,15 @@ export async function getStaticProps(context) {
 
 export default function LocationDisplayPage({ location }) {
   return (
-
     <div className={styles.location_page_wrapper}>
       <div className={styles.location_page}>
         <div className={styles.left_div}>
           <img src={location[0].image_url} className={styles.image}></img>
           <h3 className={styles.heading}>Categories:</h3>
           <section className={styles.categories}>
-
-          
             {location[0].categories.map((category) => (
-                <p>{(category.charAt(9).toUpperCase() + category.slice(10))}</p>
+              <p>{category.charAt(9).toUpperCase() + category.slice(10)}</p>
             ))}
-
-          
           </section>
           <h3 className={styles.heading}>Amenities:</h3>
           <section className={styles.amenities}>
@@ -66,15 +63,15 @@ export default function LocationDisplayPage({ location }) {
                   className={styles.icon}
                   src={`/amenities_images/${amenity}.png`}
                 />
-                <p>{(amenity.charAt(10).toUpperCase() + amenity.slice(11)).replaceAll(/_/g, ' ')}</p>
+                <p>
+                  {(
+                    amenity.charAt(10).toUpperCase() + amenity.slice(11)
+                  ).replaceAll(/_/g, ' ')}
+                </p>
               </div>
             ))}
           </section>
-          <div className={styles.report}>
-            <p>Report any issues with this page</p>
-            <img src="/report_flag.png" />
-          </div>
-
+          <Modal />
         </div>
         <div className={styles.right_div}>
           <h1 className={styles.title}>{location[0].location_name}</h1>
@@ -83,11 +80,13 @@ export default function LocationDisplayPage({ location }) {
           <p>{location[0].town}</p>
           <p>{location[0].region}</p>
           <p>{location[0].postcode}</p> */}
-          <p>{location[0].street} <br></br>
-          
-          {location[0].town}<br></br>
-          {/* {location[0].region}<br></br> */}
-          {location[0].postcode}</p>
+          <p>
+            {location[0].street} <br></br>
+            {location[0].town}
+            <br></br>
+            {/* {location[0].region}<br></br> */}
+            {location[0].postcode}
+          </p>
           <h3 className={styles.heading}>Description:</h3>
           <p>{location[0].location_description}</p>
           <div className={styles.map_container}>
@@ -96,6 +95,5 @@ export default function LocationDisplayPage({ location }) {
         </div>
       </div>
     </div>
-
   );
 }
