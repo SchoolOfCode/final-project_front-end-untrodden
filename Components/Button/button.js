8f5cf4b7672c7f9ab24ab97bb3c0e9332f0e593a
glynk <glyn_knight@hotmail.com>
2022-08-03 16:45:36 +0100

    Hotfix Filter

--- Components/Button/button.js
+++ Components/Button/button.js
@@ -1,4 +1,4 @@
 import styles from '../../styles/button.module.css'
-export default function Button ({label}){
-    return <button className={styles.button}><strong>{label}</strong></button>
+export default function Button ({onClick, label}){
+    return <button onClick={onClick} className={styles.button}><strong>{label}</strong></button>
 }
