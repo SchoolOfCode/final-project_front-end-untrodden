16f64d1c33ef72b04410ad29e0db74d3c5d882b0
danielhawkesford <daniel.hawkesford@gmail.com>
2022-08-04 10:41:39 +0100

    Added text box when no results are found

--- Components/Card Display/cardDisplay.js
+++ Components/Card Display/cardDisplay.js
@@ -6,9 +6,16 @@ import styles from "../../styles/CardDisplay.module.css";
 export default function CardDisplay({ allLocationData }) {
   return (
     <div className={styles.display_container}>
-      {allLocationData.map((location) => (
-        <LocationCard key={location.location_id} location={location} />
-      ))}
+      {allLocationData.length === 0 
+        ? <div className={styles.noResultsContainer}>
+            <p className={styles.noResults}>
+              Sorry, no results found.
+              <br/><br/>
+              Please try searching again with different filters.
+            </p>
+          </div>
+        : allLocationData.map((location) => <LocationCard key={location.location_id} location={location} />)
+      }
     </div>
   );
 }
