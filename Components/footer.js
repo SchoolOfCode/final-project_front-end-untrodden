21decc8cbaf7eeacaedd06d51308b7e5004b7bf2
TomBooth <thomas.alexander.booth@gmail.com>
2022-08-08 14:55:43 +0100

    .

--- Components/footer.js
+++ Components/footer.js
@@ -3,8 +3,7 @@ import styles from "../styles/footer.module.css"
 export default function Footer() {
   return (
     <div className={styles.footer}>
-      <p>©Untrodden by Undrodded™ 2022
-      Built using Next.Js</p>
+      <p>Untrodden© Built by Undrodded™ using Next.Js 2022</p>
     </div>
   );
 }
