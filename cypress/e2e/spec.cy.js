9ed7abee4eb23ec5637f17587782feffce4cbcb6
glynk <glyn_knight@hotmail.com>
2022-08-10 22:36:45 +0100

    Small changes to route of tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -6,20 +6,21 @@ describe('Navigation', () => {
     cy.visit('http://localhost:3000/')
 
     // Find a link with an href attribute containing "about" and click it
-    cy.get('a[href*="locations"]').click()
+    cy.get('button').contains('Start Exploring').click()
 
     // The new url should include "/about"
     cy.url().should('include', '/locations')
 
-    // The new page should contain an h1 with "About page"
+    // Show data has loaded from the date base
     cy.get('a[href*="location/1"]').contains('More Info').click()
 
+    // Shows location page displays and data has been passed down
     cy.get('h1').contains('Old Pale')
 
-    cy.get('a[href*="/"]').contains('Home').click()
-
+    //Shows nav bar links work
     cy.get('a[href*="/addlocation"]').contains('Add Location').click()
 
+    // Shows user have to be signed in to add location 
     cy.contains('Log in first!')
   })
 
