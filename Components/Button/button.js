863d12a68213e4bac81ce9c5cef8382754afd271
TomBooth <thomas.alexander.booth@gmail.com>
2022-08-10 14:22:32 +0100

    refresh

--- Components/Button/button.js
+++
@@ -1,4 +0,0 @@
-import styles from '../../styles/button.module.css'
-export default function Button ({onClick, label}){
-    return <button onClick={onClick} className={styles.button}><strong>{label}</strong></button>
-}
--- Components/footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <div className="footer">
-      <p>Copyright Untrodded by Undrodden 2022</p>
-    </div>
-  );
-}
