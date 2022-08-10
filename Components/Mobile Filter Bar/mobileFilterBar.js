53e33716190503890e9bc882f929f14bbd2fa80b
glynk <glyn_knight@hotmail.com>
2022-08-10 08:59:47 +0100

    Fix Filter alignment

--- Components/Mobile Filter Bar/mobileFilterBar.js
+++ Components/Mobile Filter Bar/mobileFilterBar.js
@@ -14,41 +14,4 @@ import RegionFilter from '../Region Filter/regionFilter';
 import CategoryFilter from '../Category Filter/categoryFilter';
 import AmenityFilter from '../Amenity Filter/amenityFilter';
 
-export default function MobileFilterBar() {
-  const [open, setOpen] = React.useState(false);
-  const [age, setAge] = React.useState('');
-
-  const handleChange = (event) => {
-    setAge(Number(event.target.value) || '');
-  };
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = (event, reason) => {
-    if (reason !== 'backdropClick') {
-      setOpen(false);
-    }
-  };
-
-  return (
-    <div>
-      <Button onClick={handleClickOpen}>Open Filters</Button>
-      <Dialog disableEscapeKeyDown open={open} onClose={handleClose}>
-        <DialogTitle>Select Filters</DialogTitle>
-        <DialogContent>
-          <Box component="form" sx={{ display: 'flex', flexWrap: 'wrap' }}>
-        <RegionFilter/>
-        <CategoryFilter/>
-        <AmenityFilter/>
-          </Box>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleClose}>Ok</Button>
-        </DialogActions>
-      </Dialog>
-    </div>
-  );
-}
+export default function MobileFilterBar() {}
